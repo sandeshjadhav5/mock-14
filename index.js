b75c8d3f989772f2b6f91596270fc76b3e400373
sandeshjadhav5 <jadhavs932@gmail.com>
2023-03-04 15:06:27 +0530

    added missing package

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const { connection } = require("./configs/db");
-const { userRouter } = require("./routes/user.routes");
+const { userRouter } = require("./routes/User.routes");
 //const { authenticate } = require("./middlewares/authenticate.middleware");
 const cors = require("cors");
 
